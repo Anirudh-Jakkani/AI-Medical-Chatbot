b43d3fe4710917857b6b1f0ab95effbbd65550ab
Aniudh Jakkani <anirudhjakkani04@gmail.com>
2026-02-02 19:47:25 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Medical-Chatbot",
+    version="0.1.0",
+    author="Anirudh Jakkani",
+    author_email="anirudhjakkani04@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
